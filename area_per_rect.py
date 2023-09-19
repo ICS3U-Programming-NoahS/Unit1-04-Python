032004744224dd31c87ff63bdbe2ebbd631e6564
Noah Smith <noah.smith@stu.ocsb.ca>
2023-09-19 03:08:31 +0000

    Created area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Noah Smith
+# Created on: Sept. 18th, 2023
+# This program calculates the area and perimeter of a rectangle
+# (With a width of 8 cm and a length of 12.5 cm)
+
+
+def main():
+    print("Width: 4 cm")
+    print("Length: 7.5 cm")
+    print("Formulas: A=LxW, P=2L + 2W")
+    print("Area: 7.5 * 4 = {}".format(7.5 * 4))
+    print("Perimeter: 2(7.5) + 2(4) = {}".format(2 * 7.5 + 2 * 4))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Noah Smith
+# Created on: Sept. 18th, 2023
+# This program does basic math.
+
+
+def main():
+    print("34 + 2.5 = {}".format(34 + 2.5))
+    print("12 - 8 = {}".format(12 - 8))
+    print("9 / 9 = {}".format(9 / 9))
+    print("8 + 7.7 * 2 = {}".format(8 + 7.7 * 2))
+    print("2^6 = {}".format(2**6))
+    print("2 / 7 = {}".format(2 / 7))
+    print("5**1/2= {}".format(5 ** (1 / 2)))
+
+
+if __name__ == "__main__":
+    main()
